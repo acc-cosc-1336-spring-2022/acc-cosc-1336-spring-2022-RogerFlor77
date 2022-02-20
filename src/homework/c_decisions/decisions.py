7435f602d74f9c9c859975817aa1496574cee329
RogerFlor77 <roger.flores@g.austincc.edu>
2022-02-19 21:05:34 -0600

    Homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,28 @@
-#
+from numbers import Rational
+
+
+option = int(input('Enter your option: '))
+total = int(input('Enter your total: '))
+
+def get_options_ratio(option, total):
+    return option / total
+
+ratio = get_options_ratio(option, total)
+
+def get_faculty_rating(ratio):
+    if ratio < 1 and ratio >= .9:
+        print ('Excellent')
+    else:
+        if ratio > .8:
+            print ('Very Good')
+        else:
+            if ratio > .7:
+                print ('Good')
+            else:
+                if ratio > .6:
+                    print ('Needs Improvement')
+                else:
+                    if ratio < .59 and num > 0:
+                        print ('Unacceptable')
+
+finalOutput = get_faculty_rating(ratio)
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,12 @@
-#
+import decisions
+
+option = int(input('Enter your option: '))
+total = int(input('Enter your total: '))
+
+decisions.get_options_ratio(option, total)
+
+result = decisions.get_options_ratio(option, total)
+
+print(result)
+
+decisions.get_faculty_rating(result)
