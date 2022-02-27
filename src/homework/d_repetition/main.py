196636fc860517730430a62868d94020cce0e1b2
RogerFlor77 <roger.flores@g.austincc.edu>
2022-02-26 23:27:20 -0600

    Homework 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,6 @@
-#
+import repetition
+
+print('Homework 3 Menu')
+print('1-Factorial')
+print('2-Sum odd numbers')
+print('3-Exit')
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,20 @@
-#
+def get_factorial(n):
+	result = 1
+	for i in range(1,n+1):
+		result = result*i
+	return result
+
+n = int(input('Enter a number: '))
+result = get_factorial(n)
+print(result)
+
+n = 20
+total_numbers = n
+
+Odd = int(input("Enter the input"))
+total = 0
+for number in range(1, Odd+1):
+    if(number % 2!=0):
+        print(number)
+        total = total + number
+print("The sum of odd numbers", total)
