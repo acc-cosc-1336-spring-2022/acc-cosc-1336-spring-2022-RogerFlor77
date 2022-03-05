e79169f0f7d568c0365b548fdc76b59f44f05d85
RogerFlor77 <roger.flores@g.austincc.edu>
2022-03-05 03:09:53 -0600

    Homework 4

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,10 @@
 import unittest
 '''
-the file in /tests/homework/d_tests_decisions/tests_repetition
+the file in /tests/homework/e_tests_functions/tests_functions
 has the test functions
 '''
 
-from tests.homework.d_repetition import tests_repetition
+from tests.homework.e_functions import tests_functions
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
+suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/homework/e_functions/tests_functions.py
+++ tests/homework/e_functions/tests_functions.py
@@ -1,1 +1,32 @@
-#
+import unittest
+from src.homework.e_functions.value_return import get_hour
+from src.homework.e_functions.value_return import get_minutes
+from src.homework.e_functions.value_return import get_seconds
+from src.homework.e_functions.value_return import time_from_utc
+
+
+class Test_Config(unittest.TestCase):
+    def get_hour(self):
+        self.assertEqual(1, get_hour(3600))
+        self.assertEqual(1, get_hour(3600))
+
+    def get_minutes(self):
+        self.assertEqual(3, get_minutes(3800))
+        self.assertEqual(0, get_minutes(3600))
+
+    def get_seconds(self):
+        self.assertEqual(20, get_seconds(3800))
+        self.assertEqual(0, get_seconds(3600))
+
+    def time_from_utc(self):
+        self.assertEqual(15, time_from_utc(-5, 20))
+
+    def time_from_utc(self):
+        self.assertEqual(14, time_from_utc(-6, 20))
+
+    def time_from_utc(self):
+        self.assertEqual(13, time_from_utc(-7, 20))
+
+    def time_from_utc(self):
+        self.assertEqual(12, time_from_utc(-8, 20))
+
