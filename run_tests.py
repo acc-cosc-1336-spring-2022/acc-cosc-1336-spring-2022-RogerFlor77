213aaecdb437dc7ef3f614596752504f84a2fb9b
RogerFlor77 <roger.flores@g.austincc.edu>
2022-04-10 17:13:31 -0500

    Homework 6

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,12 @@
 import unittest
 '''
-the file in /tests/homework/h_tests_strings/tests_strings
+the file in /tests/homework/h_tests_strings/tests_dictionares_and_sets
 has the test functions
 '''
 
-from tests.homework.h_strings import tests_strings
+from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_strings)
+suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 #test
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,23 @@
-#
+import unittest 
+
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+
+    def test_p_distance(self):
+        dna1 = ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']
+        dna2 = ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C']
+        self.assertEqual(0.4, get_p_distance(dna1, dna2))
+
+    def test_get_p_distance_matrix(self):
+        dataset = [ ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A'],
+                    ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C']
+                    ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T,' 'T', 'T']
+                    ['G', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']  ]
+
+        p_distance_matrix = [   [0.0, 0.4, 0.1, 0.1],
+                                [0.4, 0.0, 0.4, 0.3],
+                                [0.1, 0.4, 0.0, 0.2],
+                                [0.1, 0.3, 0.2, 0.0]    ]
+        self.assertEqual(p_distance_matrix, get_p_distance_matrix(dataset))
