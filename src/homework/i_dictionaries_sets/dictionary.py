eabb8954d61da62d7393e158401fc57662d61b99
RogerFlor77 <roger.flores@g.austincc.edu>
2022-04-10 17:17:44 -0500

    Homework 6

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,22 @@
-#
+def get_p_distance(dna1, dna2):
+    list_dna1 = list(dna1)
+    list_dna2 = list(dna2)
+    counter = 0
+    for i in range(len(list_dna1)):
+        if list_dna1[i] != list_dna1[i]:
+            counter += .1
+    return counter
+
+def get_p_distance_matrix(ddna1, ddna2, ddna3, ddna4):
+    list_dna1 = list(ddna1)
+    list_dna2 = list(ddna2)
+    list_dna3 = list(ddna3)
+    list_dna4 = list(ddna4)
+    new_dna1 = list(round(float(get_p_distance(list_dna1, list_dna2))), 1), round(float(get_p_distance(list_dna1, list_dna2)), 1)
+    round(float(get_p_distance(list_dna1, list_dna3), 1), round(float(get_p_distance(list_dna1, list_dna4), 1)))
+    new_dna2 = list((get_p_distance(list_dna2, list_dna1))), (float(get_p_distance(list_dna2, list_dna2))),
+    get_p_distance(list_dna2, list_dna3), round(get_p_distance(list_dna2, list_dna4), 1)
+    new_dna3 = list((get_p_distance(list_dna3, list_dna1)), get_p_distance(list_dna3, list_dna4))
+    new_dna4 = list((get_p_distance(list_dna4, list_dna1), round(get_p_distance(list_dna4, list_dna2)), 2),
+    get_p_distance(list_dna4, list_dna3), (float(get_p_distance(list_dna4, list_dna4))))
+    return new_dna1, new_dna2, new_dna3, new_dna4
--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,32 @@
-#
+import dictionary
+
+menu_selection = "y"
+while menu_selection == "y":
+    print('\nMenu\n1-Get p disance matrix\n2-Exit')
+    selection = int(input('Please select menu item 1 or 2: '))
+
+    if selection == 1: 
+        print('Enter your DNA Strings')
+        dataset = []
+        again = "y"
+        while again == 'y':
+            DNA_string = input('Enter a DNA String:')
+            DNA_list = list(DNA_string)
+            dataset.append(DNA_list)
+            print('Do you want to add another?')
+            again = input('Enter "y" for yes, n for no: ')
+        print('Here are the strings')
+        for string in dataset:
+            print(string)
+        print('The p distance matrix is:')
+        p_distance_matrix = dictionary.get_p_distance_matrix(dataset)
+        for row in p_distance_matrix:
+            for item in row:
+                print(format(item, '8.3f'), end = ' ')
+            print('')
+
+    elif selection == 2: 
+        print('Program is done')
+
+    else:
+        print("Invalid Entry")
